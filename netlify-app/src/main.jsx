8568006a0cc4705cd49a292d5133be3f680f09d0
Omar Abul-Azayem <omarabulazayem@gmail.com>
2026-09-17 15:02:58 +0300

    Load child dashboard styles

--- netlify-app/src/main.jsx
+++ netlify-app/src/main.jsx
@@ -4,6 +4,7 @@ import RootRouter from "./RootRouter.jsx";
 import "./styles.css";
 import "./ui-enhancements.css";
 import "./memory-game.css";
+import "./child-dashboard.css";
 
 const configuredBase = (import.meta.env.BASE_URL || "/").replace(/\/$/, "");
 const basePath = configuredBase === "/" ? "" : configuredBase;
